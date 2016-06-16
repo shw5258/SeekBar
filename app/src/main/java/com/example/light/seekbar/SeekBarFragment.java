6658220ac585ebb7f85982745548374f5674c6c7
hyunwoo <shw5258@naver.com>
2016-06-16 11:02:19 +0900

    listener for seekbar

--- app/src/main/java/com/example/light/seekbar/SeekBarFragment.java
+++ app/src/main/java/com/example/light/seekbar/SeekBarFragment.java
@@ -8,18 +8,27 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 
 import com.example.light.seekbar.dummy.DummyContent;
 import com.example.light.seekbar.dummy.DummyContent.DummyItem;
 
 public class SeekBarFragment extends Fragment {
 
+    private static final String KEY_CHECKED_BOXES = "key_checked_boxes";
     private OnListFragmentInteractionListener mListener;
+    private CheckBox[] mCheckBoxes;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_seekbar_list, container, false);
+
+        mCheckBoxes = new CheckBox[DummyContent.ITEMS.size()];
+        boolean[] checkedBoxes = new boolean[mCheckBoxes.length];
+        if (savedInstanceState != null && savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES) != null){
+            checkedBoxes = savedInstanceState.getBooleanArray(KEY_CHECKED_BOXES);
+        }
 
         Context context = view.getContext();
         RecyclerView recyclerView = (RecyclerView) view;
@@ -28,6 +37,17 @@ public class SeekBarFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        boolean[] stateOfCheckBoxes = new boolean[mCheckBoxes.length];
+        int i = 0;
+        for (CheckBox checkBox : mCheckBoxes) {
+            stateOfCheckBoxes[i] = checkBox.isChecked();
+            i++;
+        }
+        outState.putBooleanArray(KEY_CHECKED_BOXES, stateOfCheckBoxes);
+        super.onSaveInstanceState(outState);
+    }
 
     @Override
     public void onDetach() {
--- app/src/main/java/com/example/light/seekbar/SeekBarRecyclerViewAdapter.java
+++ app/src/main/java/com/example/light/seekbar/SeekBarRecyclerViewAdapter.java
@@ -1,13 +1,17 @@
 package com.example.light.seekbar;
 
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.light.seekbar.SeekBarFragment.OnListFragmentInteractionListener;
 import com.example.light.seekbar.dummy.DummyContent.DummyItem;
+
+import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
 import java.util.List;
 
@@ -30,15 +34,34 @@ public class SeekBarRecyclerViewAdapter extends RecyclerView.Adapter<SeekBarRecy
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).leftTaste);
-        holder.mContentView.setText(mValues.get(position).rightTaste);
+        holder.mLeftTaste.setText(mValues.get(position).leftTaste);
+        holder.mRightTaste.setText(mValues.get(position).rightTaste);
 
+        //이부분에다가 어떠한 listener든지 다 붙일 수 있다. seekbar에도 붙이고
+        //check 에도 붙이고 다 붙여보자..
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (null != mListener) {
                     mListener.onListFragmentInteraction(holder.mItem);
                 }
+            }
+        });
+
+        holder.mSeekBar.setOnProgressChangeListener(new DiscreteSeekBar.OnProgressChangeListener() {
+            @Override
+            public void onProgressChanged(DiscreteSeekBar seekBar, int value, boolean fromUser) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(DiscreteSeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(DiscreteSeekBar seekBar) {
+
             }
         });
     }
@@ -50,15 +73,17 @@ public class SeekBarRecyclerViewAdapter extends RecyclerView.Adapter<SeekBarRecy
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
-        public final TextView mIdView;
-        public final TextView mContentView;
+        public final TextView mLeftTaste;
+        public final TextView mRightTaste;
+        public final DiscreteSeekBar mSeekBar;
         public DummyItem mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            mIdView = (TextView) view.findViewById(R.id.leftTaste);
-            mContentView = (TextView) view.findViewById(R.id.rightTaste);
+            mLeftTaste = (TextView) view.findViewById(R.id.leftTaste);
+            mRightTaste = (TextView) view.findViewById(R.id.rightTaste);
+            mSeekBar = (DiscreteSeekBar) view.findViewById(R.id.seek1);
         }
     }
 }
